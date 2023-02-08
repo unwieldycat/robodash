a4aa2695030519bac7d20b7290f6e7ddc2ee5389
Thurston Yates <thurstonalexyates@gmail.com>
2023-02-08 15:58:18 -0500

    Remove "dashboard" element

--- src/pml/dashboard.cpp
+++ src/pml/dashboard.cpp
@@ -1,16 +1,12 @@
 #include "pml/dashboard.hpp"
 #include "pml/auton_selector.hpp"
 
-lv_obj_t *dashboard;
-
 lv_res_t auton_btn_action(lv_obj_t *obj) {
 	pml::auton_selector::do_selection();
 	return LV_RES_OK;
 }
 
 void create_dashboard() {
-	dashboard = lv_obj_create(lv_scr_act(), NULL);
-
 	// Actions list label
 	lv_obj_t *actions_label = lv_label_create(lv_scr_act(), NULL);
 	lv_obj_align(actions_label, NULL, LV_ALIGN_IN_TOP_RIGHT, -128, 32);
@@ -22,7 +18,6 @@ void create_dashboard() {
 	lv_obj_align(actions_list, NULL, LV_ALIGN_IN_BOTTOM_RIGHT, -48, -32);
 
 	lv_obj_t *auton_sel_btn = lv_list_add(actions_list, NULL, "Auton selection", auton_btn_action);
-	lv_obj_set_free_num(auton_sel_btn, 0);
 }
 
 void pml::dashboard::initialize() { create_dashboard(); }
