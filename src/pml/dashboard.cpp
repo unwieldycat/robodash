924b891d9580685c3070b1e7d919741e4856e2fa
Thurston Yates <thurstonalexyates@gmail.com>
2023-02-19 22:20:33 -0500

    Change button array position
    Move buttons to bottom of screen instead of on side to replicate layout
    of llemu

--- src/pml/dashboard.cpp
+++ src/pml/dashboard.cpp
@@ -1,4 +1,5 @@
 #include "dashboard.hpp"
+#include "display/lv_core/lv_obj.h"
 
 // =============================== Variables =============================== //
 
@@ -11,12 +12,15 @@ lv_obj_t *actions_list;
 
 void pml::init() {
 	dashboard = lv_cont_create(lv_scr_act(), NULL);
-	lv_obj_set_size(dashboard, 240, 480);
+	lv_obj_set_size(dashboard, 480, 240);
 	lv_obj_align(dashboard, NULL, LV_ALIGN_CENTER, 0, 0);
 
-	actions_list = lv_list_create(dashboard, NULL);
-	lv_obj_set_size(actions_list, 128, 224);
-	lv_obj_align(actions_list, NULL, LV_ALIGN_IN_TOP_RIGHT, -8, 8);
+	// FIXME: Children dont have same width or expand as much as possible
+	actions_list = lv_cont_create(dashboard, NULL);
+	lv_obj_set_height(actions_list, 32);
+	lv_cont_set_layout(actions_list, LV_LAYOUT_ROW_M);
+	lv_cont_set_fit(actions_list, true, false);
+	lv_obj_align(actions_list, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, -8);
 }
 
 lv_res_t act_btn_action(_lv_obj_t *obj) {
@@ -27,6 +31,12 @@ lv_res_t act_btn_action(_lv_obj_t *obj) {
 
 void pml::add_action_btn(std::string label, std::function<void()> action) {
 	actions.push_back(action);
-	lv_obj_t *new_btn = lv_list_add(actions_list, NULL, label.c_str(), act_btn_action);
+	lv_obj_t *new_btn = lv_btn_create(actions_list, NULL);
+	lv_obj_set_size(new_btn, 96, 32);
 	lv_obj_set_free_num(new_btn, actions.size() - 1);
+	lv_btn_set_action(new_btn, LV_BTN_ACTION_CLICK, act_btn_action);
+	lv_btn_set_fit(new_btn, true, false);
+	lv_obj_t *btn_label = lv_label_create(new_btn, NULL);
+	lv_label_set_text(btn_label, label.c_str());
+	lv_obj_align(actions_list, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, -8);
 }
