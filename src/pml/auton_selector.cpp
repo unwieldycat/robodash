6683752aec536ac4fd33e1a85b14a5dab2b7b599
Thurston Yates <thurstonalexyates@gmail.com>
2023-03-07 21:09:45 -0500

    Don't load new screen for selector and fix styles

--- src/pml/auton_selector.cpp
+++ src/pml/auton_selector.cpp
@@ -1,4 +1,6 @@
 #include "auton_selector.hpp"
+#include "common/styles.hpp"
+#include "pros/misc.hpp"
 
 // =============================== Variables =============================== //
 
@@ -8,8 +10,7 @@ bool selection_done = false;
 bool selection_running = false;
 
 lv_style_t win_style;
-lv_obj_t *main_scr;
-lv_obj_t *select_scr;
+lv_obj_t *select_cont;
 
 // =============================== Selection =============================== //
 
@@ -25,21 +26,27 @@ lv_res_t done_act(lv_obj_t *obj) {
 	return LV_RES_OK;
 }
 
+bool comp_started() {
+	return (pros::competition::is_connected() && !pros::competition::is_disabled());
+}
+
 void pml::selector::do_selection() {
-	if (selection_running || !pros::competition::is_disabled()) return;
+	if (selection_running || comp_started()) return;
 	selection_running = true;
 
-	main_scr = lv_scr_act();
-	select_scr = lv_obj_create(NULL, NULL);
-	lv_scr_load(select_scr);
+	init_styles();
 
-	lv_obj_t *title_label = lv_label_create(select_scr, NULL);
+	select_cont = lv_cont_create(lv_scr_act(), NULL);
+	lv_obj_set_size(select_cont, 480, 240);
+	lv_obj_set_style(select_cont, &bg_style);
+
+	lv_obj_t *title_label = lv_label_create(select_cont, NULL);
 	lv_label_set_text(title_label, "Select autonomous routine");
 	lv_label_set_align(title_label, LV_LABEL_ALIGN_CENTER);
 	lv_obj_set_width(title_label, 232);
 	lv_obj_align(title_label, NULL, LV_ALIGN_IN_TOP_LEFT, 8, 16);
 
-	lv_obj_t *routine_list = lv_list_create(select_scr, NULL);
+	lv_obj_t *routine_list = lv_list_create(select_cont, NULL);
 	lv_obj_set_size(routine_list, 232, 144);
 	lv_obj_align(routine_list, NULL, LV_ALIGN_IN_TOP_LEFT, 8, 48);
 
@@ -59,7 +66,7 @@ void pml::selector::do_selection() {
 	lv_obj_set_free_num(nothing_btn, -1);
 	lv_btn_set_action(nothing_btn, LV_BTN_ACTION_CLICK, &r_select_act);
 
-	lv_obj_t *done_btn = lv_btn_create(select_scr, NULL);
+	lv_obj_t *done_btn = lv_btn_create(select_cont, NULL);
 	lv_obj_set_size(done_btn, 232, 32);
 	lv_obj_align(done_btn, NULL, LV_ALIGN_IN_BOTTOM_LEFT, 8, -8);
 	lv_btn_set_action(done_btn, LV_BTN_ACTION_CLICK, &done_act);
@@ -67,12 +74,11 @@ void pml::selector::do_selection() {
 	lv_label_set_text(done_label, "Done");
 
 	// Wait for user to be done
-	while (!selection_done || !pros::competition::is_disabled()) {
+	while (!selection_done || comp_started()) {
 		pros::delay(100);
 	}
 
-	lv_scr_load(main_scr);
-	lv_obj_del(select_scr);
+	lv_obj_del(select_cont);
 
 	selection_done = false; // Set back to false if want to re-select
 	selection_running = false;
@@ -80,8 +86,7 @@ void pml::selector::do_selection() {
 
 void pml::selector::exit_selection() {
 	if (!selection_running) return;
-	lv_scr_load(main_scr);
-	lv_obj_del(select_scr);
+	lv_obj_del(select_cont);
 }
 
 // ============================= Other Methods ============================= //
