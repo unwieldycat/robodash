025c7a1c09e4dbd57d8c079de04d927f69c58caa
Thurston Yates <thurstonalexyates@gmail.com>
2023-04-14 22:55:02 -0400

    Isolate styles for readability

--- src/pml/auton_selector.cpp
+++ src/pml/auton_selector.cpp
@@ -15,12 +15,105 @@ lv_obj_t *select_cont;
 lv_obj_t *selected_label;
 lv_obj_t *saved_toast;
 
+// ============================== LVGL Styles ============================== //
+
 lv_color_t bg_color = lv_color_hsv_to_rgb(SELECTOR_HUE, 50, 15);
 lv_color_t border_color = lv_color_hsv_to_rgb(SELECTOR_HUE, 25, 50);
 lv_color_t shade_color = lv_color_hsv_to_rgb(SELECTOR_HUE, 50, 25);
 lv_color_t primary_color = lv_color_hsv_to_rgb(SELECTOR_HUE, 50, 100);
 lv_color_t primary_color_dark = lv_color_hsv_to_rgb(SELECTOR_HUE, 50, 50);
 lv_color_t text_color = lv_color_hsv_to_rgb(SELECTOR_HUE, 10, 100);
+
+lv_style_t bg_style;
+lv_style_t list_style;
+lv_style_t list_btn_style_rel;
+lv_style_t list_btn_style_pr;
+lv_style_t round_btn_style_rel;
+lv_style_t round_btn_style_pr;
+lv_style_t outline_round_btn_style_rel;
+lv_style_t outline_round_btn_style_pr;
+lv_style_t small_text;
+
+bool styles_initialized;
+void init_styles() {
+	if (styles_initialized) return;
+
+	// Background style
+	lv_style_copy(&bg_style, &lv_style_plain);
+	bg_style.body.border.width = 0;
+	bg_style.body.radius = 0;
+	bg_style.body.main_color = bg_color;
+	bg_style.body.grad_color = bg_color;
+	bg_style.text.color = text_color;
+
+	// List style
+	lv_style_copy(&list_style, &lv_style_pretty);
+	list_style.body.border.color = border_color;
+	list_style.body.border.width = 1;
+	list_style.body.border.opa = LV_OPA_COVER;
+	list_style.body.radius = 2;
+	list_style.body.grad_color = bg_color;
+	list_style.body.main_color = bg_color;
+	list_style.body.padding.ver = 0;
+	list_style.body.padding.hor = 8;
+	list_style.body.padding.inner = 0;
+
+	// List button released style
+	lv_style_copy(&list_btn_style_rel, &lv_style_plain);
+	list_btn_style_rel.body.border.width = 1;
+	list_btn_style_rel.body.border.color = border_color;
+	list_btn_style_rel.body.border.part = LV_BORDER_BOTTOM;
+	list_btn_style_rel.body.radius = 0;
+	list_btn_style_rel.body.main_color = bg_color;
+	list_btn_style_rel.body.grad_color = bg_color;
+	list_btn_style_rel.body.padding.ver = 16;
+	list_btn_style_rel.text.color = text_color;
+
+	// List button pressed style
+	lv_style_copy(&list_btn_style_pr, &list_btn_style_rel);
+	list_btn_style_pr.body.main_color = shade_color;
+	list_btn_style_pr.body.grad_color = shade_color;
+	list_btn_style_pr.body.radius = 0;
+	list_btn_style_pr.body.padding.ver = 16;
+	list_btn_style_pr.text.color = text_color;
+
+	// Released rounded button style
+	lv_style_copy(&round_btn_style_rel, &lv_style_plain);
+	round_btn_style_rel.body.main_color = primary_color;
+	round_btn_style_rel.body.grad_color = primary_color;
+	round_btn_style_rel.body.radius = 16;
+	round_btn_style_rel.image.color.full = bg_color.full;
+
+	// Pressed rounded button style
+	lv_style_copy(&round_btn_style_pr, &lv_style_plain);
+	round_btn_style_pr.body.main_color = primary_color_dark;
+	round_btn_style_pr.body.grad_color = primary_color_dark;
+	round_btn_style_pr.body.radius = 16;
+	round_btn_style_pr.image.color.full = bg_color.full;
+
+	// Released outline rounded button style
+	lv_style_copy(&outline_round_btn_style_rel, &lv_style_plain);
+	outline_round_btn_style_rel.body.border.color = border_color;
+	outline_round_btn_style_rel.body.border.opa = LV_OPA_COVER;
+	outline_round_btn_style_rel.body.border.width = 1;
+	outline_round_btn_style_rel.body.main_color = bg_color;
+	outline_round_btn_style_rel.body.grad_color = bg_color;
+	outline_round_btn_style_rel.body.radius = 16;
+	outline_round_btn_style_rel.image.color.full = text_color.full;
+
+	// Pressed outline rounded button style
+	lv_style_copy(&outline_round_btn_style_pr, &outline_round_btn_style_rel);
+	outline_round_btn_style_pr.body.main_color = shade_color;
+	outline_round_btn_style_pr.body.grad_color = shade_color;
+
+	// Small text style
+	small_text.text.color = text_color;
+	small_text.text.opa = LV_OPA_COVER;
+	small_text.text.font = &lv_font_dejavu_10;
+	small_text.text.letter_space = 1;
+
+	styles_initialized = true;
+}
 
 // ============================= SD Card Saving ============================= //
 
@@ -121,60 +214,17 @@ void selector::do_selection() {
 	if (selection_running || comp_started()) return;
 	selection_running = true;
 
-	// Main object
-
-	static lv_style_t bg_style;
-	lv_style_copy(&bg_style, &lv_style_plain);
-	bg_style.body.border.width = 0;
-	bg_style.body.radius = 0;
-	bg_style.body.main_color = bg_color;
-	bg_style.body.grad_color = bg_color;
-	bg_style.text.color = text_color;
+	init_styles();
 
 	select_cont = lv_cont_create(lv_scr_act(), NULL);
 	lv_obj_set_size(select_cont, 480, 240);
 	lv_obj_set_style(select_cont, &bg_style);
-
-	// View title
 
 	lv_obj_t *title_label = lv_label_create(select_cont, NULL);
 	lv_label_set_text(title_label, "Select autonomous routine");
 	lv_label_set_align(title_label, LV_LABEL_ALIGN_CENTER);
 	lv_obj_set_width(title_label, 232);
 	lv_obj_align(title_label, NULL, LV_ALIGN_IN_TOP_LEFT, 8, 16);
-
-	// Routine list
-
-	static lv_style_t list_style;
-	lv_style_copy(&list_style, &lv_style_pretty);
-	list_style.body.border.color = border_color;
-	list_style.body.border.width = 1;
-	list_style.body.border.opa = LV_OPA_COVER;
-	list_style.body.radius = 2;
-	list_style.body.grad_color = bg_color;
-	list_style.body.main_color = bg_color;
-	list_style.body.padding.ver = 0;
-	list_style.body.padding.hor = 8;
-	list_style.body.padding.inner = 0;
-
-	static lv_style_t list_btn_style_rel;
-	lv_style_copy(&list_btn_style_rel, &lv_style_plain);
-	list_btn_style_rel.body.border.width = 1;
-	list_btn_style_rel.body.border.color = border_color;
-	list_btn_style_rel.body.border.part = LV_BORDER_BOTTOM;
-	list_btn_style_rel.body.radius = 0;
-	list_btn_style_rel.body.main_color = bg_color;
-	list_btn_style_rel.body.grad_color = bg_color;
-	list_btn_style_rel.body.padding.ver = 16;
-	list_btn_style_rel.text.color = text_color;
-
-	static lv_style_t list_btn_style_pr;
-	lv_style_copy(&list_btn_style_pr, &list_btn_style_rel);
-	list_btn_style_pr.body.main_color = shade_color;
-	list_btn_style_pr.body.grad_color = shade_color;
-	list_btn_style_pr.body.radius = 0;
-	list_btn_style_pr.body.padding.ver = 16;
-	list_btn_style_pr.text.color = text_color;
 
 	lv_obj_t *routine_list = lv_list_create(select_cont, NULL);
 	lv_obj_set_size(routine_list, 228, 184);
@@ -183,15 +233,12 @@ void selector::do_selection() {
 	lv_list_set_style(routine_list, LV_LIST_STYLE_BTN_REL, &list_btn_style_rel);
 	lv_list_set_style(routine_list, LV_LIST_STYLE_BTN_PR, &list_btn_style_pr);
 
-	// Selected routine label
-
 	selected_label = lv_label_create(select_cont, NULL);
 	lv_label_set_align(selected_label, LV_LABEL_ALIGN_CENTER);
 	lv_label_set_text(selected_label, "No routine\nselected");
 	lv_obj_align(selected_label, NULL, LV_ALIGN_CENTER, 120, 0);
 
 	// Add routines to list
-
 	for (selector::Routine routine : routines) {
 		// Store current position in vector
 		static int r_index = 0;
@@ -203,46 +250,11 @@ void selector::do_selection() {
 		r_index++;
 	}
 
-	// Add empty routine to list
-
 	lv_obj_t *nothing_btn = lv_list_add(routine_list, NULL, "Nothing", r_select_act);
 	lv_obj_set_free_num(nothing_btn, -1);
 	lv_btn_set_style(nothing_btn, LV_BTN_STYLE_REL, &list_btn_style_rel);
 	lv_btn_set_style(nothing_btn, LV_BTN_STYLE_PR, &list_btn_style_pr);
 	lv_btn_set_action(nothing_btn, LV_BTN_ACTION_CLICK, &r_select_act);
-
-	// Create rounded button styles
-
-	static lv_style_t round_btn_style_rel;
-	lv_style_copy(&round_btn_style_rel, &lv_style_plain);
-	round_btn_style_rel.body.main_color = primary_color;
-	round_btn_style_rel.body.grad_color = primary_color;
-	round_btn_style_rel.body.radius = 16;
-	round_btn_style_rel.image.color.full = bg_color.full;
-
-	static lv_style_t round_btn_style_pr;
-	lv_style_copy(&round_btn_style_pr, &lv_style_plain);
-	round_btn_style_pr.body.main_color = primary_color_dark;
-	round_btn_style_pr.body.grad_color = primary_color_dark;
-	round_btn_style_pr.body.radius = 16;
-	round_btn_style_pr.image.color.full = bg_color.full;
-
-	static lv_style_t outline_round_btn_style_rel;
-	lv_style_copy(&outline_round_btn_style_rel, &lv_style_plain);
-	outline_round_btn_style_rel.body.border.color = border_color;
-	outline_round_btn_style_rel.body.border.opa = LV_OPA_COVER;
-	outline_round_btn_style_rel.body.border.width = 1;
-	outline_round_btn_style_rel.body.main_color = bg_color;
-	outline_round_btn_style_rel.body.grad_color = bg_color;
-	outline_round_btn_style_rel.body.radius = 16;
-	outline_round_btn_style_rel.image.color.full = text_color.full;
-
-	static lv_style_t outline_round_btn_style_pr;
-	lv_style_copy(&outline_round_btn_style_pr, &outline_round_btn_style_rel);
-	outline_round_btn_style_pr.body.main_color = shade_color;
-	outline_round_btn_style_pr.body.grad_color = shade_color;
-
-	// Make done & save button
 
 	lv_obj_t *done_btn = lv_btn_create(select_cont, NULL);
 	lv_obj_set_size(done_btn, 224, 32);
@@ -252,12 +264,6 @@ void selector::do_selection() {
 	lv_btn_set_style(done_btn, LV_BTN_STYLE_PR, &round_btn_style_pr);
 	lv_obj_t *done_img = lv_img_create(done_btn, NULL);
 	lv_img_set_src(done_img, SYMBOL_OK);
-
-	lv_style_t small_text;
-	small_text.text.color = text_color;
-	small_text.text.opa = LV_OPA_COVER;
-	small_text.text.font = &lv_font_dejavu_10;
-	small_text.text.letter_space = 1;
 
 	if (pros::usd::is_installed()) {
 		sdconf_load();
@@ -281,14 +287,12 @@ void selector::do_selection() {
 		lv_img_set_src(save_img, SYMBOL_SAVE);
 	}
 
-	// Wait for user to be done
-
+	// Wait for user to be done or for match to start
 	while (!selection_done || comp_started()) {
 		pros::delay(100);
 	}
 
 	// Delete screen and reset variables to default
-
 	lv_obj_del(select_cont);
 	selection_done = false;
 	selection_running = false;
