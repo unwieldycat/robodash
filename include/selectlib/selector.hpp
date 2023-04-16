755866ce5e32ad5ddf0371eb585cbcf100375991
Thurston Yates <thurstonalexyates@gmail.com>
2023-04-16 16:54:20 -0400

    Use correct doxygen format

--- include/selectlib/selector.hpp
+++ include/selectlib/selector.hpp
@@ -9,14 +9,14 @@
 namespace selector {
 
 /**
- * \brief Autonomous routine object
+ * @brief Autonomous routine object
  */
 struct Routine {
 	/**
-	 * \brief Construct a new Routine object
+	 * @brief Construct a new Routine object
 	 *
-	 * \param name Name of routine
-	 * \param action Routine action
+	 * @param name Name of routine
+	 * @param action Routine action
 	 */
 	Routine(std::string name, std::function<void()> action) : name(name), action(action) {}
 
@@ -25,33 +25,33 @@ struct Routine {
 };
 
 /**
- * \brief Initialize autonomous manager
- * \param autons Vector of Routine objects
+ * @brief Initialize autonomous manager
+ * @param autons Vector of Routine objects
  */
 void add_autons(std::vector<Routine> autons);
 
 /**
- * \brief Run auton selection
+ * @brief Run auton selection
  */
 void do_selection();
 
 /**
- * \brief End selection if still running. Workaround since UI doesn't clear due to task being
+ * @brief End selection if still running. Workaround since UI doesn't clear due to task being
  * forcibly ended before the code can do it automatically
  */
 void exit_selection();
 
 /**
- * \brief Run selected auton
+ * @brief Run selected auton
  */
 void do_auton();
 
 namespace theme {
 
 /**
- * \brief Set the hue of the UI
+ * @brief Set the hue of the UI
  *
- * \param hue [0..359]
+ * @param hue [0..359]
  */
 void set_hue(int hue);
 
