3a80104f2a96131ddcdb633c4e2f068ab7fd0b4e
Thurston Yates <thurstonalexyates@gmail.com>
2023-04-21 21:31:25 -0400

    Make screensaver
    Create screensaver stubs for  implementation

---
+++ include/selectlib/screensaver.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "api.hpp"
+
+namespace sl {
+namespace screensaver {
+
+/**
+ * @brief Set the screensaver image (240x480 LVGL format)
+ *
+ * @param path Path to file
+ */
+void set_image(std::string path);
+
+/**
+ * @brief Set the timeout before screensaver
+ *
+ * @param time Time in seconds
+ */
+void set_timeout(int time);
+
+/**
+ * @brief Activate screensaver
+ */
+void activate();
+
+/**
+ * @brief Exit screensaver
+ */
+void exit();
+
+} // namespace screensaver
+} // namespace sl
---
+++ src/selectlib/screensaver.cpp
@@ -0,0 +1,8 @@
+#include "screensaver.hpp"
+
+// TODO: Implement screensaver
+
+void sl::screensaver::set_image(std::string path);
+void sl::screensaver::set_timeout(int time);
+void sl::screensaver::activate();
+void sl::screensaver::exit();
