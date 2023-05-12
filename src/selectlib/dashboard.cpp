e4b22a06de2497084336e29411987816c80924f2
Thurston Yates <thurstonalexyates@gmail.com>
2023-05-12 11:40:55 -0400

    Fix styles and symbols

--- src/selectlib/dashboard.cpp
+++ src/selectlib/dashboard.cpp
@@ -12,13 +12,13 @@ std::vector<pros::Motor *> motors;
 void init_dashboard() {
 	dashboard_cont = lv_obj_create(lv_scr_act());
 	lv_obj_set_size(dashboard_cont, 480, 240);
-	// lv_obj_set_style(dashboard_cont, &bg_style);
+	lv_obj_add_style(dashboard_cont, &bg_style, 0);
 
 	// TODO: Refactor all of this
 	lv_obj_t *bat_cont = lv_obj_create(dashboard_cont);
 	lv_obj_set_size(bat_cont, 104, 32);
 	lv_obj_align(bat_cont, LV_ALIGN_TOP_RIGHT, -8, 8);
-	// lv_obj_set_style(bat_cont, &transp_style);
+	lv_obj_add_style(bat_cont, &transp_style, 0);
 	//  lv_btn_set_layout(bat_cont, LV_LAYOUT_OFF);
 
 	lv_obj_t *bat_label = lv_label_create(bat_cont);
@@ -26,7 +26,7 @@ void init_dashboard() {
 	lv_obj_align(bat_label, LV_ALIGN_LEFT_MID, 12, 0);
 
 	lv_obj_t *bat_img = lv_img_create(bat_cont);
-	// lv_img_set_src(bat_img, SYMBOL_BATTERY_FULL);
+	lv_img_set_src(bat_img, LV_SYMBOL_BATTERY_FULL);
 	lv_obj_align(bat_img, LV_ALIGN_RIGHT_MID, -12, 0);
 }
 
