104f2cb22c37c968f8b0a6c59dd5d29ce994158f
Thurston Yates <thurstonalexyates@gmail.com>
2023-06-10 14:34:28 -0400

    Window -> View

--- src/selectlib/views/devices.cpp
+++ src/selectlib/views/devices.cpp
@@ -6,9 +6,9 @@ std::vector<pros::Motor *> motors;
 
 // ============================= Core Functions ============================= //
 
-gui::DevicesWindow::DevicesWindow() : Window("Devices"){};
+gui::DevicesView::DevicesView() : View("Devices"){};
 
-void gui::DevicesWindow::initialize() {
+void gui::DevicesView::initialize() {
 	lv_obj_add_style(this->obj, &style_bg, 0);
 
 	lv_obj_t *label = lv_label_create(this->obj);
@@ -16,10 +16,10 @@ void gui::DevicesWindow::initialize() {
 	lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 }
 
-void gui::DevicesWindow::refresh() {}
+void gui::DevicesView::refresh() {}
 
 // ========================== Dashboard Functions ========================== //
 
-void gui::DevicesWindow::add_motors(std::vector<pros::Motor *> mtrs) {
+void gui::DevicesView::add_motors(std::vector<pros::Motor *> mtrs) {
 	motors.insert(motors.end(), mtrs.begin(), mtrs.end());
 }
