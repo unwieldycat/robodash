8b965dd2b0887072711fbec3bfcce29d0ccfe417
Thurston Yates <thurstonalexyates@gmail.com>
2023-06-10 17:06:47 -0400

    Add default bg style

--- src/selectlib/gui.cpp
+++ src/selectlib/gui.cpp
@@ -29,6 +29,7 @@ gui::View::View(std::string name) : name(name) {
 	this->obj = lv_obj_create(lv_scr_act());
 	lv_obj_set_size(this->obj, lv_pct(100), lv_pct(100));
 	lv_obj_add_flag(this->obj, LV_OBJ_FLAG_HIDDEN);
+	lv_obj_add_style(this->obj, &style_bg, 0);
 }
 
 gui::View::~View() { lv_obj_del(this->obj); }
