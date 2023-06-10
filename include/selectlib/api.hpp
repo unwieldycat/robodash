9d82d92ffb7cd599d801ddcc879a7e31ebecd94e
Thurston Yates <thurstonalexyates@gmail.com>
2023-06-10 09:26:42 -0400

    Fix api header includes

--- include/selectlib/api.hpp
+++ include/selectlib/api.hpp
@@ -7,8 +7,8 @@
 #include <stdio.h>
 #include <string>
 
-#include "dashboard.hpp"
 #include "gui.hpp"
 #include "screensaver.hpp"
-#include "selector.hpp"
 #include "styles.hpp"
+#include "views/dashboard.hpp"
+#include "views/selector.hpp"
