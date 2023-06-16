241107ed3c45bbdfd89c58109c8748e4b0723d56
Thurston Yates <thurstonalexyates@gmail.com>
2023-06-16 13:52:57 -0400

    Extended API header for internal stuff

---
+++ include/unwieldy-ui/apix.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file apix.hpp
+ *
+ * Extended API header for unwieldy-ui.
+ *
+ * Includes internally-used headers, such as styles.
+ */
+
+#pragma once
+
+#include "api.hpp"
+#include "internal/styles.hpp"
