1758eaf2082189d4116b62715fd58ee995f6ad8d
Thurston Yates <thurstonalexyates@gmail.com>
2023-06-16 13:52:40 -0400

    Style -> theme, and file description

--- include/unwieldy-ui/api.hpp
+++ include/unwieldy-ui/api.hpp
@@ -1,9 +1,17 @@
+/**
+ * @file api.hpp
+ *
+ * API header for unwieldy-ui.
+ *
+ * Includes all necessary functions for programmers.
+ */
+
 #pragma once
 
 #define UWUI_VERSION "2.0.0-dev"
 
 #include "gui.hpp"
 #include "screensaver.hpp"
-#include "styles.hpp"
+#include "theme.hpp"
 #include "views/devices.hpp"
 #include "views/selector.hpp"
