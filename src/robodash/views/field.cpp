a3a8764d9b11011c8ab89edd338581dee7abfe53
Thurston Yates <thurstonalexyates@gmail.com>
2023-10-06 10:36:23 -0400

    Implement functionality

--- src/robodash/views/field.cpp
+++ src/robodash/views/field.cpp
@@ -1,18 +1,41 @@
 #include "field.hpp"
 
-lv_obj_t *field_canvas;
+#define FIELD_SIZE 192
+
+lv_obj_t *field;
 lv_obj_t *arrow;
+
+double scale = 1;
+
+double pos_x;
+double pos_y;
+double hdg;
 
 // ============================= Core Functions ============================= //
 
 gui::FieldView::FieldView() : View("Field") {}
 
-void gui::FieldView::refresh() {}
+void gui::FieldView::refresh() {
+	lv_img_set_angle(arrow, hdg * 10);
+	lv_obj_set_pos(arrow, (pos_x / scale * FIELD_SIZE), (pos_y / scale * FIELD_SIZE));
+}
 
 void gui::FieldView::initialize() {
-	field_canvas = lv_obj_create(this->obj);
-	arrow = lv_img_create(field_canvas);
+	field = lv_img_create(this->obj);
+	lv_obj_set_size(field, FIELD_SIZE, FIELD_SIZE);
+
+	arrow = lv_img_create(field);
 	lv_img_set_src(arrow, LV_SYMBOL_DUMMY); // temp
 }
 
 // ============================ Field Functions ============================ //
+
+// This function does not render the arrow to minimize performance loss in
+// odometry code
+void gui::FieldView::set_pos(double x, double y, double hdg) {
+	pos_x = x;
+	pos_y = x;
+	hdg = hdg;
+}
+
+void gui::FieldView::set_scale(double max) { scale = max; }
