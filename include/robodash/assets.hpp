6c941bb1dcdf655bc84c24e718187a1149c06e52
Thurston Yates <thurstonalexyates@gmail.com>
2023-10-20 22:57:03 -0400

    Create workaround for missing fonts in liblvgl

--- include/robodash/assets.hpp
+++ include/robodash/assets.hpp
@@ -2,4 +2,6 @@
 #include "apix.hpp"
 
 extern lv_font_t source_code_pro_16;
+extern lv_font_t montserrat_12;
+extern lv_font_t montserrat_16;
 extern lv_img_t stack;
--- src/robodash/styles/text.cpp
+++ src/robodash/styles/text.cpp
@@ -17,6 +17,13 @@ void _init_style_text() {
 	lv_style_set_text_font(&style_text_mono, &source_code_pro_16);
 	lv_style_set_text_letter_space(&style_text_mono, 1);
 
+	// Small text
+	lv_style_init(&style_text_small);
+	lv_style_set_text_color(&style_text_small, color_text);
+	lv_style_set_text_opa(&style_text_small, LV_OPA_COVER);
+	lv_style_set_text_font(&style_text_small, &montserrat_12);
+	lv_style_set_text_letter_space(&style_text_small, 1);
+
 	// Medium text
 	lv_style_init(&style_text_medium);
 	lv_style_set_text_color(&style_text_medium, color_text);
@@ -28,7 +35,7 @@ void _init_style_text() {
 	lv_style_init(&style_text_large);
 	lv_style_set_text_color(&style_text_large, color_text);
 	lv_style_set_text_opa(&style_text_large, LV_OPA_COVER);
-	lv_style_set_text_font(&style_text_large, &lv_font_montserrat_20);
+	lv_style_set_text_font(&style_text_large, &montserrat_16);
 
 	// Text align
 	lv_style_init(&style_text_centered);
