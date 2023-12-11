da47616821306c1481a4f405fdf96b50d42fe2ac
Thurston Yates <thurstonalexyates@gmail.com>
2023-12-10 22:35:52 -0500

    Fix console not able to scroll

--- src/robodash/views/console.cpp
+++ src/robodash/views/console.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 
 lv_obj_t *output;
+lv_obj_t *output_cont;
 std::ostringstream stream;
 
 // ============================= Core Functions ============================= //
@@ -12,12 +13,19 @@ rd::Console::Console(std::string name) {
 
 	lv_obj_set_style_bg_color(view->obj, color_bg, 0);
 
+	// FIXME: Make output_cont scroll to bottom every refresh
+	output_cont = lv_obj_create(view->obj);
+	lv_obj_set_width(output_cont, 464);
+	lv_obj_set_height(output_cont, LV_SIZE_CONTENT);
+	lv_obj_align(output_cont, LV_ALIGN_CENTER, 0, 0);
+	lv_obj_add_style(output_cont, &style_transp, 0);
+
 	output = lv_label_create(view->obj);
-	lv_obj_set_size(output, 464, 224);
-	lv_obj_align(output, LV_ALIGN_CENTER, 0, 0);
+	lv_obj_align(output, LV_ALIGN_TOP_LEFT, 0, 0);
 	lv_obj_add_style(output, &style_transp, 0);
 	lv_obj_add_style(output, &style_text_mono, 0);
 	lv_label_set_recolor(output, true);
+	lv_label_set_long_mode(output, LV_LABEL_LONG_WRAP);
 }
 
 // =========================== Console Functions =========================== //
