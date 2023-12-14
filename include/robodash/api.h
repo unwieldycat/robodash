da2e98a10ad4ac7e85a636e10f4bf422c3e884bb
Thurston Yates <thurstonalexyates@gmail.com>
2023-12-13 22:09:17 -0500

    Further ensure compatibility

--- include/robodash/api.h
+++ include/robodash/api.h
@@ -7,15 +7,32 @@
 
 #pragma once
 
+#define ROBODASH
 #define RD_VERSION_MAJOR 2
 #define RD_VERSION_MINOR 0
 #define RD_VERSION_PATCH 0
 
 #include "liblvgl/lvgl.h"
 
+// ========================== Check Compatibility ========================== //
+
 #if (LV_VERSION_CHECK(8, 3, 0) == 0)
 #error "LVGL version incompatible with robodash (Requires >= 8.3.X)"
 #endif
+
+#ifndef LV_FONT_MONTSERRAT_12
+#error "LVGL install incompatible with robodash, missing font: Montserrat 12"
+#endif
+
+#ifndef LV_FONT_MONTSERRAT_14
+#error "LVGL install incompatible with robodash, missing font: Montserrat 14"
+#endif
+
+#ifndef LV_FONT_MONTSERRAT_16
+#error "LVGL install incompatible with robodash, missing font: Montserrat 16"
+#endif
+
+// ============================ Include Library ============================ //
 
 #include "core.h"
 
