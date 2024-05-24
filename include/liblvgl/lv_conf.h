f94f8238ff14a7b2302c497066080f2b96c98e49
Thurston Yates <thurstonalexyates@gmail.com>
2024-05-23 20:08:30 -0400

    Upgrade to PROS 4.1

--- include/liblvgl/lv_conf.h
+++ include/liblvgl/lv_conf.h
@@ -759,8 +759,6 @@ typedef void * lv_obj_user_data_t;
 #  define LV_TILEVIEW_DEF_ANIM_TIME   300
 #endif
 
-#define CONFIG_LV_USE_GIF 1
-
 /*Window (dependencies: lv_cont, lv_btn, lv_label, lv_img, lv_page)*/
 #define LV_USE_WIN      1
 
