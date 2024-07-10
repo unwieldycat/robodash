9c9130ed69a30c5e77e592a552ae551e70067cf2
Thurston Yates <thurstonalexyates@gmail.com>
2024-07-10 15:58:51 -0400

    Create alert functions

---
+++ include/robodash/alert.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "robodash/view.hpp"
+#include <string>
+
+namespace rd {
+
+/**
+ * Dispatch an alert
+ *
+ * @param message Message to display
+ */
+void alert(std::string message);
+
+/**
+ * Dispatch an alert that links back to a view
+ *
+ * @param message Message to display
+ * @param view View to link back to
+ */
+void alert(std::string message, View view);
+
+} // namespace rd
---
+++ src/robodash/alert.cpp
@@ -0,0 +1,9 @@
+#include "alert.hpp"
+
+void rd::alert(std::string message) {
+	// TODO: Implement
+}
+
+void rd::alert(std::string message, rd::View view) {
+	// TODO: Implement
+}
