76b8c1e69269e2b7b9c590ce0df61d74dcd8c4ef
Thurston Yates <thurstonalexyates@gmail.com>
2024-07-17 21:53:55 -0500

    Move up directory before calling Doxygen

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,4 +64,4 @@ import subprocess, os
 read_the_docs_build = os.environ.get("READTHEDOCS", None) == "True"
 
 if read_the_docs_build:
-    subprocess.call("doxygen", shell=True)
+    subprocess.call("cd ..; doxygen", shell=True)
