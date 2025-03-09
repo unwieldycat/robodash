69829bcde2673e300a84ecf5e7f9642c37f3c42f
Thurston Yates <thurstonalexyates@gmail.com>
2025-03-09 16:16:46 -0400

    Create basic setting UIs

--- src/robodash/widgets/settings.cpp
+++ src/robodash/widgets/settings.cpp
@@ -9,27 +9,84 @@ rd::Settings::Settings(std::string name) : view(name) {
 	lv_obj_align(settings_cont, LV_ALIGN_CENTER, 0, 0);
 	lv_obj_add_style(settings_cont, &style_transp, 0);
 	lv_obj_set_layout(settings_cont, LV_LAYOUT_FLEX);
+	lv_obj_set_flex_flow(settings_cont, LV_FLEX_FLOW_COLUMN);
+	lv_obj_set_flex_align(
+	    settings_cont, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_START
+	);
 }
 
-bool &rd::Settings::toggle(std::string key, bool default_value) {
-	lv_obj_t *setting_cont = lv_obj_create(view);
-	lv_obj_set_size(setting_cont, lv_pct(100), 30);
+lv_obj_t *rd::Settings::create_setting_cont(std::string key) {
+	lv_obj_t *setting_cont = lv_obj_create(settings_cont);
+	lv_obj_set_size(setting_cont, lv_pct(80), 32);
 	lv_obj_add_style(setting_cont, &style_transp, 0);
+
+	lv_obj_t *setting_label = lv_label_create(setting_cont);
+	lv_obj_add_style(setting_label, &style_text_large, 0);
+	lv_obj_align(setting_label, LV_ALIGN_LEFT_MID, 8, 0);
+	lv_label_set_text(setting_label, key.c_str());
+
+	return setting_cont;
 }
 
-std::string &rd::Settings::dropdown(
+std::shared_ptr<bool> rd::Settings::toggle(std::string key, bool default_value) {
+	lv_obj_t *setting_cont = create_setting_cont(key);
+	lv_obj_t *setting_toggle = lv_switch_create(setting_cont);
+	lv_obj_set_size(setting_toggle, 48, 24);
+	lv_obj_align(setting_toggle, LV_ALIGN_RIGHT_MID, -8, 0);
+
+	return std::make_shared<bool>(true);
+}
+
+std::shared_ptr<std::string> rd::Settings::dropdown(
     std::string key, std::vector<std::string> values, std::string default_value
 ) {
-	return default_value;
+	lv_obj_t *setting_cont = create_setting_cont(key);
+	lv_obj_t *setting_dropdown = lv_dropdown_create(setting_cont);
+	lv_obj_set_size(setting_dropdown, 96, 24);
+	lv_obj_align(setting_dropdown, LV_ALIGN_RIGHT_MID, -8, 0);
+
+	return std::make_shared<std::string>("stub");
 }
 
-double &
+std::shared_ptr<double>
 rd::Settings::slider(std::string key, double min, double max, double step, double default_value) {
-	return default_value;
+	lv_obj_t *setting_cont = create_setting_cont(key);
+	lv_obj_t *setting_slider = lv_slider_create(setting_cont);
+	lv_obj_set_size(setting_slider, 96, 8);
+	lv_obj_align(setting_slider, LV_ALIGN_RIGHT_MID, -8, 0);
+
+	return std::make_shared<double>(0.0);
 }
 
-int &rd::Settings::increment(std::string key, int min, int max, int default_value) {
-	return default_value;
+std::shared_ptr<int> rd::Settings::increment(std::string key, int min, int max, int default_value) {
+	lv_obj_t *setting_cont = create_setting_cont(key);
+
+	lv_obj_t *increment_cont = lv_obj_create(setting_cont);
+	lv_obj_set_size(increment_cont, 96, 24);
+	lv_obj_align(increment_cont, LV_ALIGN_RIGHT_MID, -8, 0);
+	lv_obj_add_style(increment_cont, &style_transp, 0);
+
+	lv_obj_t *down_btn = lv_btn_create(increment_cont);
+	lv_obj_set_size(down_btn, 24, 24);
+	lv_obj_align(down_btn, LV_ALIGN_LEFT_MID, 8, 0);
+	lv_obj_add_style(down_btn, &style_btn_outline, 0);
+	lv_obj_t *down_label = lv_label_create(down_btn);
+	lv_label_set_text(down_label, LV_SYMBOL_MINUS);
+	lv_obj_align(down_label, LV_ALIGN_CENTER, 0, 0);
+
+	lv_obj_t *number = lv_label_create(increment_cont);
+	lv_label_set_text_fmt(number, "%d", default_value);
+	lv_obj_align(number, LV_ALIGN_CENTER, 0, 0);
+
+	lv_obj_t *up_btn = lv_btn_create(increment_cont);
+	lv_obj_set_size(up_btn, 24, 24);
+	lv_obj_align(up_btn, LV_ALIGN_RIGHT_MID, -8, 0);
+	lv_obj_add_style(up_btn, &style_btn_outline, 0);
+	lv_obj_t *up_label = lv_label_create(up_btn);
+	lv_label_set_text(up_label, LV_SYMBOL_PLUS);
+	lv_obj_align(up_label, LV_ALIGN_CENTER, 0, 0);
+
+	return std::make_shared<int>(0);
 }
 
 void rd::Settings::focus() { view.focus(); }
